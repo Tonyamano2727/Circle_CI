d8f74c1d0b9110606b67d7df190ee58f9282b20c
Tonyamano2727 <toanb3074@gmail.com>
2024-12-28 04:18:36 +0700

    updates

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello world");
-  console.log('gigi');
+  console.log('gigigigig');
 });
 
 app.get("/product", async (req, res) => {
